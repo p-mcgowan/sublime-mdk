9f10f6f2af7a40beb5f6017773b47a2a5146704a
pat@home <mcgowanapat@gmail.com>
2020-11-28 21:01:42 +0100

    so close - almost got dlls working

--- mdk_build.py
+++ mdk_build.py
@@ -10,6 +10,7 @@ import glob
 import json
 import shutil
 import traceback
+from datetime import datetime
 
 # def copy(view, text):
 #     sublime.set_clipboard(text)
@@ -27,72 +28,143 @@ class MdkBuildCommand(sublime_plugin.WindowCommand):
     mdk_root = os.path.dirname(os.path.abspath(__file__))
     manifest_filename = 'mdk.sublime-settings'
     panel = None
-    working_dir = None
+    manifest_dir = None
+    manifest_dir = None
     file_regex = None
-    panel = None
     encoding = None
     build_dir = None
     output = None
     main = None
     files = None
     thumb = None
+    se_game_dir = None
+
+    panel_lock = threading.Lock()
+    killed = False
+    proc = None
+    encoding = 'utf-8'
 
     def run(self, **kwargs):
         try:
-            if not self.import_settings():
-                return 1
+            self.build()
 
-            targets = self.collect_build_files()
-            if targets is None:
-                self.log("No files to build")
-                return 1
-
-            self.log("will build: \n  {}".format("\n  ".join(targets)))
-            self.build(targets)
-
-        except Exception as Argument:
+        except:
             self.log(traceback.format_exc())
 
-    def build(self, targets):
-        self.log("building")
-        out_path = os.path.realpath(self.build_dir)
-        shutil.rmtree(out_path, ignore_errors=True)
+    def build(self):
+        if not self.import_settings():
+            return 1
+
+        targets = self.collect_build_files()
+        if targets is None:
+            self.log("No files to build")
+            return 1
+
+        compile_files = self.prepare(targets)
+        self.log("\n".join(compile_files))
+        bat_file = self.compile(compile_files)
+        exitcode = self.run_bat(bat_file)
+
+        if exitcode == 0:
+            self.onSuccess(targets)
+            self.log('Build success')
+        else:
+            self.log('Build failed')
+
+    def setup_build_panel(self, working_dir=None):
+        if working_dir == None:
+            working_dir = self.manifest_dir
+
+        with self.panel_lock:
+            self.panel = self.window.create_output_panel("panel")
+            settings = self.panel.settings()
+            settings.set('result_file_regex', r"^(.*\.cs)\((\d*),(\d*)\): .*")
+            settings.set('result_line_regex', r"^(.*\.cs)\((\d*),(\d*)\): .*")
+            settings.set('result_base_dir', self.working_dir)
+
+            preferences = sublime.load_settings('Preferences.sublime-settings')
+            if preferences.get('show_panel_on_build'):
+                self.window.run_command('show_panel', { "panel": "output.panel" })
+
+    def compile(self, files):
+        bat_out = os.path.join(self.build_dir, "compile.bat")
+        bat_tpl = os.path.join(self.mdk_root, 'MDK/compile.bat')
+
+        try:
+            os.remove(bat_out)
+        except:
+            pass
+
+        with open(bat_out, "a") as bat_file:
+            with open(bat_tpl, "r") as bat_head:
+                content = " ".join(
+                    bat_head.read().replace('MDK_ROOT', self.mdk_root)
+                    .replace('SE_GAME_DIR', self.se_game_dir).split("\n")
+                    )
+                bat_file.write(content)
+
+            bat_file.write('"{}"'.format('" "'.join(files)))
+
+        return bat_out
+
+    def onSuccess(self, files):
+        for file in ['Bootstrapper.exe', 'compile.bat']:
+            try:
+                os.remove(os.path.join(self.build_dir, file))
+            except:
+                pass
+
+        with open(self.output, 'wb') as out_fd:
+            for f in files:
+                with open(f, 'rb') as in_fd:
+                    shutil.copyfileobj(in_fd, out_fd)
+
+    def prepare(self, targets):
+        shutil.rmtree(self.build_dir, ignore_errors=True)
+        compile_files = []
 
         def to_build_dir(full_path):
-            shared_root = os.path.commonprefix([out_path, full_path])
+            shared_root = os.path.commonprefix([self.build_dir, full_path])
             return full_path.replace(shared_root, '')
 
-        head = os.path.join(self.mdk_root, 'lib/head.cs')
-        tail = os.path.join(self.mdk_root, 'lib/tail.cs')
+        with open(os.path.join(self.mdk_root, 'MDK/head.cs')) as head_file:
+            head = head_file.read().rstrip()
 
-        # wrap and copy to build
+        with open(os.path.join(self.mdk_root, 'MDK/tail.cs')) as tail_file:
+            tail = tail_file.read().rstrip()
+
         for target in targets:
-            out_file = os.path.join(out_path, to_build_dir(target))
+            out_file = os.path.join(self.build_dir, to_build_dir(target))
+
             try:
                 os.makedirs(os.path.dirname(out_file))
             except Exception:
                 pass
 
-            self.log(out_file)
-            with open(out_file, 'wb') as out_fd:
-                for f in [head, target, tail]:
-                    with open(f, 'rb') as in_fd:
-                        shutil.copyfileobj(in_fd, out_fd)
+            with open(out_file, 'w') as output:
+                output.write(head)
+                with open(target) as in_fd:
+                    output.write(in_fd.read())
+                output.write(tail)
+
+            compile_files.append(out_file)
+
+        compile_files.reverse() # make main last
+
+        return compile_files
 
     def collect_build_files(self):
         files = {}
-        output = os.path.realpath(self.output)
-        main = os.path.realpath(self.main)
-        build_dir = os.path.realpath(self.build_dir)
 
         def add_file(file_path):
             filename = os.path.realpath(file_path)
             _, file_extension = os.path.splitext(filename)
+
             if (
                 file_extension != '.cs' or
-                filename == output or
-                filename == main or
-                filename == build_dir
+                filename == self.output or
+                filename == self.main or
+                filename == self.build_dir
                 ):
                 return
 
@@ -107,8 +179,8 @@ class MdkBuildCommand(sublime_plugin.WindowCommand):
                     add_file(file)
 
         targets = list(files.keys())
-        if os.path.isfile(main):
-            targets.insert(0, main)
+        if os.path.isfile(self.main):
+            targets.insert(0, self.main)
 
         return targets if len(targets) else None
 
@@ -116,23 +188,21 @@ class MdkBuildCommand(sublime_plugin.WindowCommand):
         window_vars = self.window.extract_variables()
         self.working_dir = window_vars['file_path']
 
-        self.file_regex = "\\(.*\\.cs\\)\\(([\\d]+),([\\d]+)\\): (.*)"
-        self.panel = self.window.create_output_panel("panel")
-
-        preferences = sublime.load_settings('Preferences.sublime-settings')
-        if preferences.get('show_panel_on_build'):
-            self.window.run_command('show_panel', { "panel": "output.panel" })
+        self.setup_build_panel(self.working_dir)
 
         manifest = self.find_manifest()
         if manifest is None:
             return False
 
-        self.encoding = manifest.get('encoding', 'utf-8')
-        self.build_dir = manifest.get('build_dir', "{}/build".format(os.getcwd()))
-        self.output = manifest.get('output', 'Script.cs')
-        self.main = manifest.get('main', 'main.cs')
-        self.files = manifest.get('files', '*')
-        self.thumb = manifest.get('thumb', True)
+        settings = self.panel.settings()
+        settings.set("result_base_dir", self.manifest_dir)
+        self.encoding = manifest.get("encoding", "utf-8")
+        self.build_dir = os.path.realpath(manifest.get("build_dir", "{}/build".format(os.getcwd())))
+        self.output = os.path.realpath(manifest.get("output", "Script.cs"))
+        self.main = os.path.realpath(manifest.get("main", "main.cs"))
+        self.files = manifest.get("files", "*")
+        self.thumb = manifest.get("thumb", True)
+        self.se_game_dir = os.path.realpath(manifest.get("se_game_dir", "c:\\program files (x86)\\steam\\SteamApps\\common\\SpaceEngineers"))
 
         return True
 
@@ -146,6 +216,7 @@ class MdkBuildCommand(sublime_plugin.WindowCommand):
                     return None
                 else:
                     with open(os.path.join(os.getcwd(), self.manifest_filename)) as settings:
+                        self.manifest_dir = here
                         return sublime.decode_value(settings.read())
 
             os.chdir('../')
@@ -153,11 +224,80 @@ class MdkBuildCommand(sublime_plugin.WindowCommand):
                 self.log("No {} found from {} to {}".format(self.manifest_filename, self.working_dir, os.getcwd()))
                 return None
 
+        os.chdir(self.manifest_dir)
+
     def log(self, msg, newline=True):
         if self.panel is not None:
-            self.panel.run_command("append", {
-                "characters": "{}{}".format(msg, "\n" if newline else None)
-            })
+            with self.panel_lock:
+                self.panel.run_command("append", {
+                    "characters": "{}{}".format(msg, "\n" if newline else None)
+                })
+
+    def run_bat(self, path_to_bat):
+        if self.proc is not None:
+            self.proc.terminate()
+            self.proc = None
+
+        SW_HIDE = 0
+        info = subprocess.STARTUPINFO()
+        info.dwFlags = subprocess.STARTF_USESHOWWINDOW
+        info.wShowWindow = SW_HIDE
+
+        self.proc = subprocess.Popen(
+            path_to_bat,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            cwd=self.build_dir,
+            startupinfo=info
+        )
+        self.killed = False
+
+        threading.Thread(target=self.read_handle, args=(self.proc.stdout,)).start()
+        return self.proc.wait()
+
+    def read_handle(self, handle):
+        chunk_size = 2 ** 13
+        out = b''
+        while True:
+            try:
+                data = os.read(handle.fileno(), chunk_size)
+                # If exactly the requested number of bytes was
+                # read, there may be more data, and the current
+                # data may contain part of a multibyte char
+                out += data
+                if len(data) == chunk_size:
+                    continue
+                if data == b'' and out == b'':
+                    raise IOError('EOF')
+                # We pass out to a function to ensure the
+                # timeout gets the value of out right now,
+                # rather than a future (mutated) version
+                self.queue_write(out.decode(self.encoding))
+                if data == b'':
+                    raise IOError('EOF')
+                out = b''
+
+            except (UnicodeDecodeError) as e:
+                self.queue_write('Error decoding output using {} - {}'.format(self.encoding, str(e)))
+                break
+
+            except (IOError):
+                if self.killed:
+                    msg = 'Cancelled'
+                else:
+                    msg = 'Finished'
+                self.queue_write('\n[{}]'.format(msg))
+                break
+
+    def queue_write(self, text):
+        sublime.set_timeout(lambda: self.log(text.replace(self.build_dir, self.manifest_dir)), 1)
+
+    def is_enabled(self, lint=False, integration=False, kill=False):
+        # The Cancel build option should only be available
+        # when the process is still running
+        if kill:
+            return self.proc is not None and self.proc.poll() is None
+        return True
 
 
 #         scope = self.view.scope_name(self.view.sel()[-1].b)
--- readme.md
+++ readme.md
@@ -4,10 +4,15 @@ Steal the MDK compiler for Visual Studio, use in sublime build to get errors and
 May need to install msbuild deps (see https://github.com/malware-dev/MDK-SE/wiki/Getting-Started)
 
 - Copy folder into sublime/packages/
+- Unzip msbuild.zip (pre-packaged dll's - prolly won't work on everyone's machine)  
+  **NOTE** make sure it extracts into the same folder (.../Packages/se-mdk/)  
+  If you messed this up, just move se-mdk/msbuild/msbuild to se-mdk/msbuild  
 - Open APPDATA/Roaming/SpaceEngineers/IngameScripts/local/script-folder
 - (optionally) add mdk.sublime-settings to the folder
 - Select Tools -> Build System -> se-mdk
 - Profit
 
-Thanks to [malware](https://github.com/malware-dev)
-Based off [mdk-se](https://github.com/malware-dev/MDK-SE)
+If you have SE installed somewhere other than c...steamapps..common whatever, then change it in the mdk.sublime-settings file
+
+Thanks to [malware](https://github.com/malware-dev)  
+Based off [mdk-se](https://github.com/malware-dev/MDK-SE)  
